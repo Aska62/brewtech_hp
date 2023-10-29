ddebd19a466aa8478481c288c1fe531ca7eb4668
Aska Takahashi <aska.jpg@gmail.com>
2023-10-29 12:47:35 +0900

    navbar adjusted for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useState } from "react";
 
 function Navbar({currentPage, logoType}) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleHamburger = () => {
+    setMenuOpen(!menuOpen);
+  }
 
   return (
     <section className='navbar'>
       <a href="/" className={`logo ${logoType}`} alt='logo' width="500" height="600"></a>
+
       <div className="nav_box">
         <a href="/" className={`nav_link`}>
           <div className="nav_current" style={{opacity: (currentPage === 'home') ? 1:0}}></div>
@@ -19,6 +25,33 @@ function Navbar({currentPage, logoType}) {
           Supports
         </a>
         <a href="/contact" className="nav_link">
+          <div className='nav_current' style={{opacity: currentPage == 'contact' ? 1:0}}></div>
+          Contact
+        </a>
+      </div>
+
+      <div className="hamburger-container">
+        <div className={`hamburger-box ${menuOpen ? 'hamburger_menu-open' : ''}`} onClick={handleHamburger}>
+          <div className="hamburger-line hamburger-line_top"></div>
+          <div className="hamburger-line hamburger-line_middle"></div>
+          <div className="hamburger-line hamburger-line_bottom"></div>
+        </div>
+      </div>
+
+      <div className={`menu-container ${menuOpen ? 'menu-container_open' : ''}`}>
+        <a href="/" className={`menu_link`}>
+          <div className="nav_current" style={{opacity: (currentPage === 'home') ? 1:0}}></div>
+          Home
+        </a>
+        <a href="/products" className={`menu_link`}>
+          <div className='nav_current' style={{opacity: (currentPage === 'products') ? 1:0}}></div>
+          Products
+        </a>
+        <a href="/supports" className={`menu_link`}>
+          <div className='nav_current' style={{opacity: currentPage == 'supports' ? 1:0}}></div>
+          Supports
+        </a>
+        <a href="/contact" className={`menu_link`}>
           <div className='nav_current' style={{opacity: currentPage == 'contact' ? 1:0}}></div>
           Contact
         </a>
