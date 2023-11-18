c15ef70497fe65aa450904e95b9148b7f42b07b6
Aska Takahashi <aska.jpg@gmail.com>
2023-11-18 17:42:29 +0900

    added swipe event to image slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,8 @@ function Home() {
   const [imageDisp, setImageDisp] = useState(1);
   const [count, setCount] = useState(0);
   const [delay, setDelay] = useState(1000);
+  const [startX, setStartX] = useState(0);
+  const [endX, setEndX] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -31,8 +33,47 @@ function Home() {
   // Click photo navigation to change photos to display
   const onPhotoNaviClick = (photoId) => {
     setImageDisp(photoId);
-    setCount(1)
+    setCount(1);
   }
+
+  // Get touch start point
+  const onTouchStart = (e) => {
+    setStartX(e.touches[0].pageX);
+  }
+
+  // Get touch end point
+  const onTouchMove = (e) => {
+    setEndX(e.changedTouches[0].pageX);
+  }
+
+  const onTouchEnd = () => {
+    const minimumDistance = 30; // Minimum swipe distance to activate swipe move
+    const distanceX = Math.abs(endX - startX); // Swipe distance
+
+    if (distanceX > minimumDistance) {
+      setCount(1);
+      if (endX > startX) {
+        // If the swipe move is from left to right
+        if (imageDisp === 3) {
+          setImageDisp(1);
+        } else {
+          setImageDisp(imageDisp+1);
+        }
+      } else {
+        // If the swipe move is from right to left
+        if (imageDisp === 1) {
+          setImageDisp(3);
+        } else {
+          setImageDisp(imageDisp-1);
+        }
+      }
+    }
+
+    setStartX(0);
+    setStartY(0);
+    setEndX(0);
+    setEndY(0);
+  };
 
   return (
     <>
@@ -40,7 +81,11 @@ function Home() {
 
       <main className="main">
         <div className="image-container">
-          <div className="image-cover image-cover_home">
+          <div className="image-cover image-cover_home"
+            onTouchStart={e => onTouchStart(e)}
+            onTouchMove={e => onTouchMove(e)}
+            onTouchEnd={onTouchEnd}
+            >
             <div className="home-heading_wrapper">
               <h2 className="home-heading" id="home-heading_1">Nature,</h2>
               <h2 className="home-heading" id="home-heading_2">Science,</h2>
@@ -51,7 +96,7 @@ function Home() {
             <div
               className={`image-box image-box_home ${imageDisp === 1 ? 'image_show':''}`}
               id="image_1">
-              </div>
+            </div>
             <div
               className={`image-box image-box_home ${imageDisp === 2 ? 'image_show':''}`}
               id="image_2"
